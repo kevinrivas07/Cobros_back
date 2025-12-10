96db1589ed261501fe5fa60a3ef81e2be7fa02e4
kevin <kevinr060799@gmail.com>
2025-12-10 09:24:47 -0500

    Se crea el archivo index.js

--- server.js
+++ server.js
@@ -4,20 +4,39 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const loanRoutes = require('./Routes/loanRoutes');
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use('/api', loanRoutes);
-
-let isConnected = false;
+let app;
+let mongoConnected = false;
 
 module.exports = async (req, res) => {
+  try {
+    // Inicializar app solo una vez
+    if (!app) {
+      app = express();
 
-  if (!isConnected) {
-    await mongoose.connect(process.env.MONGO_URI);
-    isConnected = true;
+      app.use(cors({
+        origin: "https://cobros-front-eta.vercel.app",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+      }));
+
+      app.use(express.json());
+      app.use("/api", loanRoutes);
+    }
+
+    // Conectar solo una vez
+    if (!mongoConnected) {
+      await mongoose.connect(process.env.MONGO_URI);
+      mongoConnected = true;
+      console.log("Conexión inicializada con MongoDB");
+    }
+
+    return app(req, res);
+
+  } catch (error) {
+    console.error("ERROR SERVERLESS =>", error);
+
+    res.status(500).json({
+      error: "Error interno del servidor",
+      details: error.message
+    });
   }
-
-  return app(req, res);
 };
