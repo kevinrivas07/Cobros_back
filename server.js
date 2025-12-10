e2ce611243bb1cd4efb94c0333db957ef6319aea
kevin <kevinr060799@gmail.com>
2025-12-10 09:22:25 -0500

    Se crea el archivo index.js

--- server.js
+++ server.js
@@ -10,13 +10,14 @@ app.use(cors());
 app.use(express.json());
 app.use('/api', loanRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log('Conectado a MongoDB');
-    app.listen(process.env.PORT, () => {
-      console.log(`Servidor corriendo en http://localhost:${process.env.PORT}`);
-    });
-  })
-  .catch(err => {
-    console.error('Error al conectar a MongoDB:', err);
-  });
+let isConnected = false;
+
+module.exports = async (req, res) => {
+
+  if (!isConnected) {
+    await mongoose.connect(process.env.MONGO_URI);
+    isConnected = true;
+  }
+
+  return app(req, res);
+};
