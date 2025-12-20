86170079488a9c4b9eae50da4ae97574a5431fa0
Kevin Rivas <kevinr060799@gmail.com>
2025-12-19 22:31:07 -0500

    Se cambia url del front

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 // Configuración de CORS dinámica
 const allowedOrigins = [
   "http://localhost:5173", 
-  "https://cobros-front-roan.vercel.app" // Tu URL de Vercel
+  "https://cobros-front-eta.vercel.app/" // Tu URL de Vercel
 ];
 
 app.use(cors({
